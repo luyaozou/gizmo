d936fef8aa2bfbe205e55f0b8f3d232bb9036327
Luyao Zou <zouluyao@hotmail.com>
2016-08-08 18:38:39 -0400

    Add output format option for xyavg

--- xyavg.py
+++ xyavg.py
@@ -33,6 +33,14 @@ def arg():
     parser.add_argument('file', nargs='+', help='files need to be averaged')
     parser.add_argument('-o', nargs=1, help='Output file name string')
     parser.add_argument('-d', action='store_true', help='delete original data')
+    fmt = parser.add_mutually_exclusive_group()
+    fmt.add_argument('-csv', action='store_true',
+                     help='Save file in commat delimited format | DEFAULT')
+    fmt.add_argument('-txt', action='store_true',
+                     help='Save file in white space delimited format')
+    fmt.add_argument('-npy', action='store_true',
+                     help='Save file in numpy binary format')
+
     return parser.parse_args()
 
 
@@ -49,10 +57,21 @@ if __name__ == "__main__":
 
     avg = average(files)    # this is a generator
 
-    for i in range(len(files)):
-        np.savetxt('{:s}-{:d}.csv'.format(out_name, i+1), next(avg),
-                   delimiter=',', fmt='%.6f')
-        print('{:s}-{:d}.csv saved.'.format(out_name, i+1))
+    if input_args.npy:
+        for i in range(len(files)):
+            np.save('{:s}-{:d}.npy'.format(out_name, i+1), next(avg))
+            print('{:s}-{:d}.npy saved.'.format(out_name, i+1))
+    else:
+        if input_args.txt:
+            delm = ' '
+            ext = 'txt'
+        else:
+            delm = ','
+            ext = 'csv'
+        for i in range(len(files)):
+            np.savetxt('{:s}-{:d}.{:s}'.format(out_name, i+1, ext), next(avg),
+                       delimiter=delm, fmt='%.6f')
+            print('{:s}-{:d}.{:s} saved.'.format(out_name, i+1, ext))
 
     if input_args.d:
         for a_file in files:
